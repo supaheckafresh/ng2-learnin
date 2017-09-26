61fc5e7a9bb685156d8e15946c62f54a1123f45f
supaheckafresh <neilstrain@gmail.com>
2017-09-26 10:12:18 -0400

    master app component and hero detail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+
+const HEROES: Hero[] = [
+  {
+    id: 11, name: 'Mr. Nice'
+  },
+  {
+    id: 12, name: 'Milly Bofiggins'
+  },
+  {
+    id: 13, name: 'Mr. Bad'
+  },
+  {
+    id: 14, name: 'Breezy'
+  },
+  {
+    id: 15, name: 'Celery'
+  },
+  {
+    id: 16, name: 'Lechuga'
+  },
+  {
+    id: 17, name: 'Pineapple Boy'
+  },
+  {
+    id: 18, name: 'Gordita'
+  },
+  {
+    id: 19, name: 'Puggles'
+  }
+];
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+      <h1>{{title}}</h1>
+      <h2>My Heroes</h2>
+      <ul class="heroes">
+          <li *ngFor="let hero of heroes"
+              [class.selected]="hero === hero"
+              (click)="onSelect(hero)">
+              <span class="badge">{{hero.id}}</span> {{hero.name}}
+          </li>
+      </ul>
+      <hero-detail [hero]="selectedHero"></hero-detail>
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
